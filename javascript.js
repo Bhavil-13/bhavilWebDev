a664697612ed8b12f450c97d1983ed3c52e15086
Bhavil-13 <bhavilsharma83@gmail.com>
2022-11-09 03:04:19 +0530

    Commit

--- javascript.js
+++ javascript.js
@@ -1,25 +1,74 @@
 "use strict";
+console.log("Welcome");
+let notesObj;
+showNotes();
 
-function sendEmail(sendersEmail, password, recieversEmail, subject, message) {
-  Email.send({
-    Host: "smtp.gmail.com",
-    Username: sendersEmail,
-    Password: password,
-    To: recieversEmail,
-    From: sendersEmail,
-    Subject: subject,
-    Body: message,
-  }).then(function (message) {
-    alert("mail sent successfully");
+document.getElementById("addBtn").addEventListener("click", function () {
+  let addTxt = document.getElementById("addTxt");
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+  notesObj.push(addTxt.value);
+  localStorage.setItem("notes", JSON.stringify(notesObj));
+  addTxt.value = "";
+  showNotes();
+});
+
+function showNotes() {
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+  let html = "";
+  notes = notes.slice(2, -2);
+  notes = notes.split(`","`);
+  notes.forEach(function (element, index) {
+    html += `
+    <div class="my-2 mx-2 noteCard card" style="width: 18rem;">
+          <div class="card-body">
+            <h5 class="card-title">Note ${index + 1}</h5>
+            <p class="card-text">${element}</p>
+            <button onclick="deleteNote(this.id)" id=${index} class="btn btn-primary">Delete Note</a>
+          </div>
+        </div>
+        `;
   });
+  let notesElm = document.getElementById("notes");
+  if (notesObj.length != 0) {
+    notesElm.innerHTML = html;
+  } else {
+    notesElm.innerHTML = `No notes`;
+  }
 }
 
-document.querySelector(".send").addEventListener("click", function () {
-  let sendersEmail = document.querySelector(".semail").value;
-  let password = document.querySelector(".password").value;
-  let recieversEmail = document.querySelector(".remail").value;
-  let subject = document.querySelector(".subject").value;
-  let message = document.querySelector(".message").value;
+function deleteNote(index) {
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
 
-  sendEmail(sendersEmail, password, recieversEmail, subject, message);
+  notesObj.splice(index, 1);
+  localStorage.setItem("notes", JSON.stringify(notesObj));
+  showNotes();
+}
+
+let searchTxt = document.getElementById("searchTxt");
+searchTxt.addEventListener("input", function () {
+  let inputVal = searchTxt.value.toLowerCase();
+  let noteCards = document.getElementsByClassName("noteCard");
+  Array.from(noteCards).forEach(function (element) {
+    let cardTxt = element.getElementsByTagName("p")[0].innerText;
+    if (cardTxt.includes(inputVal)) {
+      element.style.display = "block";
+    } else {
+      element.style.display = "none";
+    }
+  });
 });
