1b6a54fb36fc2a40594a9d01bfe0f2cfb28b2c2c
Bhavil-13 <bhavilsharma83@gmail.com>
2022-11-21 04:34:36 +0530

    Update

--- feedJS.js
+++ feedJS.js
@@ -1,24 +1,36 @@
+"use strict";
+
 let contentArr = [];
 let html = "";
+function showBlog() {
+  contentArr.forEach(element => {
+    html += `<div class="card mb-3 container my-4">
+    <div class="card-body">
+      <h5 class="card-title">${element.Heading}</h5>
+      <div id="feedContent">
+      <p class="card-text">${element.Content}</p>
+    </div>
+    </div>
+  </div>`;
+
+    let blogHTML = document.querySelector("#blogCard");
+    blogHTML.innerHTML = html;
+  });
+}
+
 const data = require("./feedData.json");
 let sizeOfFile = Object.keys(data).length;
 for (const key in data) {
   if (Object.hasOwnProperty.call(data, key)) {
-    // const element = data[key];
     contentArr.push(data[key]);
   }
 }
-// console.log(typeof contentArr[0]);
-contentArr.forEach(element => {
-  html += `<div class="card mb-3 container my-4">
-  <div class="card-body">
-    <h5 class="card-title">${element.Heading}</h5>
-    <div id="feedContent">
-    <p class="card-text">${element.Content}</p>
-  </div>
-  </div>
-</div>`;
-  let blogHTML = document.getElementById("blogCard");
-  blogHTML.innerHTML = html;
-});
-console.log(html);
+
+showBlog();
+function browserOrServer() {
+  if (typeof window !== "undefined") {
+    console.log("You are on the browser");
+  } else {
+    console.log("You are on the server");
+  }
+}
