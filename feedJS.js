3611ce89ccb8728dfe3eec1cc7b8ac3cb14eba04
Bhavil-13 <bhavilsharma83@gmail.com>
2022-11-20 23:30:58 +0530

    update

--- feedJS.js
+++ feedJS.js
@@ -1,6 +1,24 @@
-const myBlogInput = require("./profileJS");
+let contentArr = [];
 let html = "";
-console.log(myBlogInput.content);
-let feedContent = document.getElementById("feedContent");
-html += `<p class="card-text">${myBlogInput.content}</p>`;
-feedContent.innerHTML = html;
+const data = require("./feedData.json");
+let sizeOfFile = Object.keys(data).length;
+for (const key in data) {
+  if (Object.hasOwnProperty.call(data, key)) {
+    // const element = data[key];
+    contentArr.push(data[key]);
+  }
+}
+// console.log(typeof contentArr[0]);
+contentArr.forEach(element => {
+  html += `<div class="card mb-3 container my-4">
+  <div class="card-body">
+    <h5 class="card-title">${element.Heading}</h5>
+    <div id="feedContent">
+    <p class="card-text">${element.Content}</p>
+  </div>
+  </div>
+</div>`;
+  let blogHTML = document.getElementById("blogCard");
+  blogHTML.innerHTML = html;
+});
+console.log(html);
--- profileJS.js
+++ profileJS.js
@@ -1,28 +1,36 @@
 "use strict";
 
 const publish = document.querySelector(".publishBlog");
-const emailID = String(document.querySelector(".userInputEmail"));
-const password = document.querySelector(".userInputPassword");
-const title = document.querySelector(".userInputTitle");
-const content = document.querySelector(".contentInput");
-const tags = document.querySelector(".tagInput");
 
 class feedContent {
-  constructor(emailID, password, title, content, tags) {
+  constructor(emailID, password, title, content) {
     this.emailID = emailID;
     this.password = password;
     this.title = title;
     this.content = content;
-    this.tags = tags;
+    // this.tags = tags;
   }
 }
+
+let tagArr = ["Entertainment", "Educational", "News"];
+let contentArr = [];
+
 if (publish) {
-  publish.addEventListener("click", function () {
+  publish.addEventListener("click", function (e) {
+    e.preventDefault();
     // 1. Put all the details in a variable, and then in an object of feedContent
-    let myBlog = new feedContent(emailID, password, title, content, tags);
-    // let myBlogString = JSON.stringify(myBlog);
-    let blogString = JSON.stringify(myBlog);
-    module.exports = blogString;
+    const emailID = document.querySelector("#inputEmail4").value;
+    const password = document.querySelector("#inputPassword4").value;
+    const title = document.querySelector("#userInputTitle").value;
+    const content = document.querySelector("#contentInput").value;
+    const tag1 = document.querySelector("#gridCheck1").value;
+    const tag2 = document.querySelector("#gridCheck2").value;
+    const tag3 = document.querySelector("#gridCheck3").value;
+    console.log(emailID, password, title, content, tag1, tag2, tag3);
+
+    let myBlog = new feedContent(emailID, password, title, content);
+    contentArr.push(JSON.stringify(myBlog));
+    localStorage.setItem("test", JSON.stringify(myBlog));
 
     // 2. Then, send all that to feedJS.js file
   });
